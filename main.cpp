6c4770a22a5c0a866ee65bef82db9825fe14e61c
Cecilulysess <cosmobserver@gmail.com>
2014-01-04 20:33:57 -0500

    running done

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 #include <thread>
 #include <chrono>
 #include <cstdlib>
+#include <string>
 using std::cout;
 using std::endl;
 
 
-int main(){
-	cout<<"Start"<<endl;
+int main(int argc, char** argv){
+	cout<<"MinerLite Started, wait for 2 seconds for running cgmienr.."<<endl;
 	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-	cout<<"2 secs later"<<endl;
-	cout<<std::system("ls -l")<<endl;
+	cout<<"Run cgminer"<<endl;
+
+	std::string cmd("~/mining/cgminer/cgminer");
+	if (argc < 2) {
+		cout<<cmd<<endl;
+	} else {
+		cout<<"Run cgminer from: "<<argv[1]<<endl;
+	}
+	
+	cout<<std::system(cmd.c_str())<<endl;
 }
